21e926b3a0cd939868e2839bff722d5be17a44db
Renan Geraldini Leão <131619149+renanjava@users.noreply.github.com>
2024-05-29 20:25:40 -0300

    Add files via upload
    tentando fazer a lógica de trocar de página (ainda estou pensando em como resolver isso)

--- index.js
+++ index.js
@@ -1,47 +1,58 @@
-fetch("https://servicodados.ibge.gov.br/api/v3/noticias/?qtd=10")
-    .then((fetchData) => {
-        return fetchData.json()
-    })
-    .then((jsonData) => {
-        jsonData.items.forEach(element => {
-            gerarConteudo(element.titulo,
-                "https://agenciadenoticias.ibge.gov.br/" + JSON.parse(element.imagens).image_intro,
-                element.introducao, element.data_publicacao, element.editorias)
-        });
-    })
+history.pushState(null, null, window.location.pathname + "?qtd=10");
 
-function gerarConteudo(titulo, imagem, introducao, data, hashtag) {
-    const ul = document.getElementById("lista-noticias")
-    const li = document.createElement("li")
-    const div = document.createElement("div")
-    div.classList.add("div-imagem-noticia")
-    const div2 = document.createElement("div")
-    div2.classList.add("div-introducao-noticia")
-    const div3 = document.createElement("div")
-    div3.classList.add("div-informacao-noticia")
-    const img = document.createElement("img")
-    const h2 = document.createElement("h2")
-    const p = document.createElement("p")
-    const p2 = document.createElement("p")
-    const p3 = document.createElement("p")
-    const button = document.createElement("button")
-    button.id = "button-leia-mais"
-    img.src = imagem
-    h2.textContent = titulo
-    p.textContent = introducao
-    p2.textContent = "#" + hashtag
-    p3.textContent = retornaDiferencaData(data)
-    button.textContent = "Leia mais"
-    div3.appendChild(p2)
-    div3.appendChild(p3)
-    div.appendChild(img)
-    div2.appendChild(h2)
-    div2.appendChild(p)
-    div2.appendChild(div3)
-    div2.appendChild(button)
-    div.appendChild(div2)
-    li.appendChild(div)
-    ul.appendChild(li)
+const urlSearchParams = new URLSearchParams(location.search)
+const quantidadeNoticias = urlSearchParams.get("qtd")
+
+insereNoticiasNaPagina()
+
+function insereNoticiasNaPagina() {
+    fetch(`https://servicodados.ibge.gov.br/api/v3/noticias/?qtd=${quantidadeNoticias}`)
+        .then((fetchData) => {
+            return fetchData.json()
+        })
+        .then((jsonData) => {
+            jsonData.items.forEach(element => {
+                gerarConteudo(element.titulo,
+                    "https://agenciadenoticias.ibge.gov.br/" +
+                    JSON.parse(element.imagens).image_intro,
+                    element.introducao,
+                    element.data_publicacao,
+                    element.editorias)
+            });
+        })
+
+    function gerarConteudo(titulo, imagem, introducao, data, editoria) {
+        const ul = document.getElementById("lista-noticias")
+        const li = document.createElement("li")
+        const div = document.createElement("div")
+        div.classList.add("div-imagem-noticia")
+        const div2 = document.createElement("div")
+        const div3 = document.createElement("div")
+        div3.classList.add("div-informacao-noticia")
+        const img = document.createElement("img")
+        const h2 = document.createElement("h2")
+        const p = document.createElement("p")
+        const p2 = document.createElement("p")
+        const p3 = document.createElement("p")
+        const button = document.createElement("button")
+        button.id = "button-leia-mais"
+        img.src = imagem
+        h2.textContent = titulo
+        p.textContent = introducao
+        p2.textContent = "#" + editoria
+        p3.textContent = retornaDiferencaData(data)
+        button.textContent = "Leia mais"
+        div3.appendChild(p2)
+        div3.appendChild(p3)
+        div.appendChild(img)
+        div2.appendChild(h2)
+        div2.appendChild(p)
+        div2.appendChild(div3)
+        div2.appendChild(button)
+        div.appendChild(div2)
+        li.appendChild(div)
+        ul.appendChild(li)
+    }
 }
 
 function retornaDiferencaData(data) {
@@ -78,8 +89,9 @@ function retornaDiferencaData(data) {
         }
 
     }
-
-
     return "Publicado há " + diferencaTempo + " " + diferencaTipo
+}
+
+function atualizaPage() {
 
 }
